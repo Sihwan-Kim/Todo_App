6e347bc595c0c5931a1e5607056871c78f757542
Sihwan Kim <sihwan72@gmail.com>
2019-06-09 01:04:26 +0900

    주석추가

--- Todo_App/TaskListTableViewCell.swift
+++ Todo_App/TaskListTableViewCell.swift
@@ -29,8 +29,9 @@ class TaskListTableViewCell: UITableViewCell {
 
     @IBAction func swCompleteChanged(_ sender: Any)
     {
-        let filter = "task_name=='\(lbTaskName.text!)'"
+        // switch 버튼의 상태를 변경하여 작업 완료 여부를 변경한다.
+        let filter = "task_name=='\(lbTaskName.text!)'"  // 정보가 변경된 Task 를 찾기위한 필터 설정
             
-        taskListProcess.completeUpdate(isComplete: !swTaskCompleate.isOn, filter: filter)
+        taskListProcess.completeUpdate(isComplete: !swTaskCompleate.isOn, filter: filter)  // 변경된 task name를 찾아서 정보를 갱신한다.
     }
 }
